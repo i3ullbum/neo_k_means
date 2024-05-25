920907b6f6d77c953368320bff04fcd954d13c0f
i3ullbum <bullbum1126@gmail.com>
2024-05-25 16:52:15 +0900

    First commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from modules.preprocessing import *
+from modules.neokmeans import * 
+from modules.portfolio import *
+from modules.metrics import *
+
+
+def main():
+    pass
+    # preprocessing
+
+
+    # neokmeans
+
+
+    # portfolio
+
+
+    # metrics
+    
+
+
+if __name__ == "__main__":
+    main()
